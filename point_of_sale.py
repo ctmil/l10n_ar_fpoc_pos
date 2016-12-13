2225ee31cb6a264de77a49e0e81ce5d3fcdb48e4
morrillo <gustavo.orrillo@moldeo.coop>
2016-12-13 14:55:10 -0300

    Fixed bug

--- point_of_sale.py
+++ point_of_sale.py
@@ -88,7 +88,7 @@ class pos_order(osv.osv):
                     "document_number": partner.document_number,
                     "responsability": responsability_map.get(partner.responsability_id.code, "F"),
                 },
-                "related_document": _("No picking"),
+                "related_document": order.picking_id.name or _("No picking"),
                 "related_document_2": "",
                 "turist_check": "",
                 "lines": [ ],
@@ -162,6 +162,134 @@ class pos_order(osv.osv):
 
         #return r
         return None 
+
+    def create_refund_from_ui_v2(self, cr, uid, orders, context=None):
+        session_obj = self.pool.get('pos.session')
+        partner_obj = self.pool.get('res.partner')
+        user_obj = self.pool.get('res.users')
+        product_obj = self.pool.get('product.product')
+        journal_obj = self.pool.get('account.journal')
+
+        #r = super(pos_order, self).create_from_ui(cr, uid, orders, context=context)
+        #for idx, _id in enumerate(r):
+	for idx in orders:
+            #data = orders[idx]['data']
+	    order = self.pool.get('pos.order').browse(cr,uid,idx)	
+            #session = session_obj.browse(cr, uid, data['pos_session_id'])
+            session = order.session_id
+            journal = session.config_id.journal_id
+            user = order.user_id
+
+            if not journal.use_fiscal_printer:
+                continue
+
+            if not journal.fiscal_printer_id:
+                raise osv.except_osv(_('Error'), _('You must set a fiscal printer for the journal'))
+
+            if not journal.fiscal_printer_state in ['ready']:
+                raise osv.except_osv(_('Error!'), _('Printer is not ready to print.'))
+
+            if not journal.fiscal_printer_fiscal_state in ['open']:
+                raise osv.except_osv(_('Error!'), _('You can\'t print in a closed printer.'))
+
+            #if not journal.fiscal_printer_paper_state in ['ok']:
+            #    raise osv.except_osv(_('Error!'), _('You can\'t print in low level of paper printer.'))
+
+            if not journal.fiscal_printer_anon_partner_id:
+                raise osv.except_osv(_('Error'), _('You must set anonymous partner to the journal.'))
+
+            partner = order.partner_id
+            if not partner:
+                partner = journal.fiscal_printer_anon_partner_id
+
+            ticket={
+                "turist_ticket": False,
+                "debit_note": False,
+                "partner": {
+                    "name": partner.name,
+                    "name_2": "",
+                    "address": partner.street,
+                    "address_2": partner.city,
+                    "address_3": partner.country_id.name,
+                    "document_type": document_type_map.get(partner.document_type_id.code, "D"),
+                    "document_number": partner.document_number,
+                    "responsability": responsability_map.get(partner.responsability_id.code, "F"),
+                },
+                "related_document": order.name or _("No related doc"),
+                "related_document_2": "",
+                "turist_check": "",
+                "lines": [ ],
+                "payments": [ ],
+                "cut_paper": True,
+                "electronic_answer": False,
+                "print_return_attribute": False,
+                "current_account_automatic_pay": False,
+                "print_quantities": True,
+                "tail_no": 1 if user.name else 0,
+                "tail_text": _("Saleman: %s") % user.name if user.name else "",
+                "tail_no_2": 0,
+                "tail_text_2": "",
+                "tail_no_3": 0,
+                "tail_text_3": "",
+            }
+            #for op1, op2, line in data['lines']:
+            for line in order.lines:
+                product = line.product_id
+                ticket["lines"].append({
+                    "item_action": "sale_item",
+                    "as_gross": False,
+                    "send_subtotal": True,
+                    "check_item": False,
+                    "collect_type": "q",
+                    "large_label": "",
+                    "first_line_label": "",
+                    "description": "",
+                    "description_2": "",
+                    "description_3": "",
+                    "description_4": "",
+                    "item_description": product.name,
+                    #"quantity": line['qty'],
+                    #"unit_price": line['price_unit'],
+                    "quantity": line.qty,
+                    "unit_price": line.price_unit,
+                    "vat_rate": 0, # TODO
+                    "fixed_taxes": 0,
+                    "taxes_rate": 0
+                })
+                if line.discount > 0: 
+			ticket["lines"].append({
+	                    "item_action": "discount_item",
+        	            "as_gross": False,
+	                    "send_subtotal": True,
+        	            "check_item": False,
+                	    "collect_type": "q",
+	                    "large_label": "",
+        	            "first_line_label": "",
+                	    "description": "",
+	                    "description_2": "",
+        	            "description_3": "",
+                	    "description_4": "",
+	                    "item_description": "%5.2f%%" % line.discount,
+        	            "quantity": line.qty,
+                	    "unit_price": line.price_unit * (line.discount/100.),
+	                    "vat_rate": 0.0, # TODO
+        	            "fixed_taxes": 0,
+                	    "taxes_rate": 0
+	                })
+            for pay in order.statement_ids:
+                payment_journal = journal_obj.browse(cr, uid, pay.journal_id.id)
+                ticket["payments"].append({
+                    "type": "pay",
+                    "extra_description": "",
+                    "description": payment_journal.name,
+                    "amount": pay.amount,
+                })
+ 
+            ticket_resp = journal.make_fiscal_refund_ticket(ticket)
+
+        #return r
+        return None 
+
 pos_order()
 
 # v:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
